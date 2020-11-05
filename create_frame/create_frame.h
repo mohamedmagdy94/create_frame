bdf3029e2697890e2a85bcb1b19357ab43d8dd4d
Mohamed ElTaweel <mohamedmagdy.9080@gmail.com>
2020-11-05 14:55:37 +0200

    Initial Commit

---
+++ create_frame/create_frame.h
@@ -0,0 +1,19 @@
+//
+//  create_frame.h
+//  create_frame
+//
+//  Created by Mohamed El-Taweel on 11/5/20.
+//  Copyright © 2020 Learning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for create_frame.
+FOUNDATION_EXPORT double create_frameVersionNumber;
+
+//! Project version string for create_frame.
+FOUNDATION_EXPORT const unsigned char create_frameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <create_frame/PublicHeader.h>
+
+
